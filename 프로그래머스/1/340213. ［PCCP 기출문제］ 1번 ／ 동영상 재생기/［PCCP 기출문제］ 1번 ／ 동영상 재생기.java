c31e49f55ae19c2fc684d703b93c4e700c498377
하승 <108933977+HaSeung2@users.noreply.github.com>
2025-01-13 22:09:53 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 9.42 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -0,0 +1,40 @@
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+class Solution {
+    public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
+        String command;
+        LocalTime videoLen = LocalTime.parse("00:"+video_len, DateTimeFormatter.ofPattern("HH:mm:ss"));
+        LocalTime posTime = LocalTime.parse("00:"+pos, DateTimeFormatter.ofPattern("HH:mm:ss"));
+        LocalTime opStartTime = LocalTime.parse("00:"+op_start, DateTimeFormatter.ofPattern("HH:mm:ss"));
+        LocalTime opEndTime = LocalTime.parse("00:"+op_end, DateTimeFormatter.ofPattern("HH:mm:ss"));
+        LocalTime zero = LocalTime.parse("00:00:00", DateTimeFormatter.ofPattern("HH:mm:ss"));
+
+        for(int i = 0; i < commands.length; i++){
+            if((posTime.isAfter(opStartTime) && posTime.isBefore(opEndTime)) || posTime.equals(opStartTime)){
+                posTime = opEndTime;
+            }
+            command = commands[i];
+            switch (command){
+                case "prev" :
+                    if(posTime.getSecond() < 10 && posTime.getMinute() == 0){
+                        posTime = zero;
+                        break;
+                    }
+                    posTime = posTime.minusSeconds(10);
+                    break;
+                case "next" :
+                    if(posTime.plusSeconds(10).isAfter(videoLen)){
+                        posTime = videoLen;
+                        break;
+                    }
+                    posTime = posTime.plusSeconds(10);
+                    break;
+            }
+            if((posTime.isAfter(opStartTime) && posTime.isBefore(opEndTime)) || posTime.equals(opStartTime)){
+                posTime = opEndTime;
+            }
+        }
+        String answer = posTime.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+        return answer.substring(3);
+    }
+}
