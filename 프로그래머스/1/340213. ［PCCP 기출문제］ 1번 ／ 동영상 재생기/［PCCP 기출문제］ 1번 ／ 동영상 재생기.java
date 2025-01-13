f592370acc5c3ba323b2760ac5ba4cd86eeafb76
하승 <108933977+HaSeung2@users.noreply.github.com>
2025-01-13 22:14:54 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 9.27 ms, Memory: 90 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -10,10 +10,10 @@ class Solution {
         LocalTime zero = LocalTime.parse("00:00:00", DateTimeFormatter.ofPattern("HH:mm:ss"));
 
         for(int i = 0; i < commands.length; i++){
+            command = commands[i];
             if((posTime.isAfter(opStartTime) && posTime.isBefore(opEndTime)) || posTime.equals(opStartTime)){
                 posTime = opEndTime;
             }
-            command = commands[i];
             switch (command){
                 case "prev" :
                     if(posTime.getSecond() < 10 && posTime.getMinute() == 0){
@@ -30,10 +30,10 @@ class Solution {
                     posTime = posTime.plusSeconds(10);
                     break;
             }
-            if((posTime.isAfter(opStartTime) && posTime.isBefore(opEndTime)) || posTime.equals(opStartTime)){
+        }
+        if((posTime.isAfter(opStartTime) && posTime.isBefore(opEndTime)) || posTime.equals(opStartTime)){
                 posTime = opEndTime;
             }
-        }
         String answer = posTime.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
         return answer.substring(3);
     }
