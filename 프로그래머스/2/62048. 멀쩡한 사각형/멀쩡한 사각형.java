1518703822e218b4288864a1f7c2786e4604c529
하승 <108933977+HaSeung2@users.noreply.github.com>
2025-01-22 14:18:26 +0900

    Update 멀쩡한 사각형.java

--- 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -1,7 +1,6 @@
 class Solution {
     public long solution(int w, int h) {
         long answer = 0;
-        int max = w * h;
         
         for(int i = 0; i < w; i++){
             double n = ((double)h * i)/w;
