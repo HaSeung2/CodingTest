99ce45ef539ce3d9c63f78b00a11c5b4f7b7645d
하승 <108933977+HaSeung2@users.noreply.github.com>
2025-01-22 14:17:31 +0900

    [level 2] Title: 멀쩡한 사각형, Time: 680.95 ms, Memory: 84.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/62048. 멀쩡한 사각형/멀쩡한 사각형.java
@@ -0,0 +1,13 @@
+class Solution {
+    public long solution(int w, int h) {
+        long answer = 0;
+        int max = w * h;
+        
+        for(int i = 0; i < w; i++){
+            double n = ((double)h * i)/w;
+            answer += (new Double(n)).longValue();
+        }
+        answer *= 2;
+        return answer;
+    }
+}
