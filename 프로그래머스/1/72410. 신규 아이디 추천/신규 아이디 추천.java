ceaaaa86654b6d798ec9acd500ccbcd33811f11d
하승 <108933977+HaSeung2@users.noreply.github.com>
2025-02-12 15:56:38 +0900

    Update 신규 아이디 추천.java

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -4,8 +4,7 @@ class Solution {
         new_id = new_id.toLowerCase()
             .replaceAll("[^a-z0-9-_.]", "")
             .replaceAll("\\.{2,}",".")
-            .replaceAll("^\\.", "")
-            .replaceAll("\\.$", "");
+            .replaceAll("^\\.|\\.$", "");
         new_id = new_id.equals("") ? "a" : new_id;
         if(new_id.length() > 15){
             new_id = new_id.substring(0,15)
