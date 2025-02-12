c4f0778d8c4dced01affedd20cca7bba897f9d75
하승 <108933977+HaSeung2@users.noreply.github.com>
2025-02-12 15:54:13 +0900

    [level 1] Title: 신규 아이디 추천, Time: 4.34 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String new_id) {
+        String answer = "";
+        new_id = new_id.toLowerCase()
+            .replaceAll("[^a-z0-9-_.]", "")
+            .replaceAll("\\.{2,}",".")
+            .replaceAll("^\\.", "")
+            .replaceAll("\\.$", "");
+        new_id = new_id.equals("") ? "a" : new_id;
+        if(new_id.length() > 15){
+            new_id = new_id.substring(0,15)
+                .replaceAll("\\.$", "");
+        }
+        while(new_id.length() < 3){
+            new_id += new_id.substring(new_id.length()-1);
+        }
+        answer = new_id;
+        return answer;
+    }
+}
