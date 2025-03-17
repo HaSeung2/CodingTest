b5d85132bfda73aaa35bda41a9e36bf9acb8bdff
하승 <108933977+HaSeung2@users.noreply.github.com>
2025-03-17 18:10:08 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 45.54 ms, Memory: 134 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        int num = 0;
+      while(num<n){
+        if(num %2 == 0){
+            answer += "수";
+        }
+          else{
+              answer += "박";
+          }
+          num++;
+      }
+        return answer;
+    }
+}
